e968ad189252689ef5be6a485a3607c75357dd56
Aymane <afirdoussi123@gmail.com>
2025-08-04 12:09:16 +0200

    adding the samping of a new train batch for alpha

--- fine_tune_glue_2.py
+++ fine_tune_glue_2.py
@@ -103,23 +103,24 @@ def train(model, loader, args):
 
             total_train_loss += loss.item()
             
-            if i % args.T == 0:
-                optimizer_alpha.step()
+            # if i % args.T == 0:
+            #     optimizer_alpha.step()
 
             logits = outputs.logits
             predictions = torch.argmax(logits, dim=-1)
             total_train_correct += (predictions == labels).sum().item()
             
-            # if i % args.T == 0: # update alpha
-            #     optimizer_alpha.zero_grad()
-            #     batch = next(iter(loader['train']))
-            #     input_ids = batch['input_ids'].to(device)
-            #     attention_mask = batch['attention_mask'].to(device)
-            #     labels = batch['label'].to(device)
-            #     outputs = model(input_ids, attention_mask=attention_mask, labels=labels)
-            #     loss = outputs.loss
-            #     loss.backward()
-            #     optimizer_alpha.step()
+            if i % args.T == 0: # update alpha
+                optimizer_alpha.zero_grad()
+                # Sample a new batch
+                batch = next(iter(loader['train']))
+                input_ids = batch['input_ids'].to(device)
+                attention_mask = batch['attention_mask'].to(device)
+                labels = batch['label'].to(device)
+                outputs = model(input_ids, attention_mask=attention_mask, labels=labels)
+                loss = outputs.loss
+                loss.backward()
+                optimizer_alpha.step()
 
             # Log batch loss less frequently or not at all, to avoid noisy charts
             if i % 10 == 0:
